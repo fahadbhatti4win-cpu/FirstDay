fedaf0c8b270df068e712dc9e2b39cc20bd25c9d
Fahad Bhatti <fahadbhatti4win@gmail.com>
2026-05-06 21:20:12 +0500

    Craete File Wednesday

--- diserolling/src/App.js
+++ diserolling/src/App.js
@@ -12,7 +12,7 @@ function App() {
      {/* <DiceRolling/> */}
      {/* <Reharts/> */}
      <FahadWork/>
-     <ZainWork/>
+     {/* <ZainWork/> */}
      <Wednesday/>
     </div>
   );
--- diserolling/src/component/Wednesday.jsx
+++ diserolling/src/component/Wednesday.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Wednesday = () => {
   return (
     <div>
-      
+      <h1>Today is Wednesday</h1>
     </div>
   )
 }
